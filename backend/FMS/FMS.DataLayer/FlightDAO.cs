d702aad999097da7c50c77f54d7469d2aea513f1
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2022-07-17 12:03:41 +0530

    FlightDAO and UserDAO implemented

--- backend/FMS/FMS.DataLayer/FlightDAO.cs
+++ backend/FMS/FMS.DataLayer/FlightDAO.cs
@@ -1,10 +1,107 @@
 ﻿using System;
+using System.Data.SqlClient;
+using FMS.Entities;
 using System.Collections.Generic;
-using System.Text;
 
 namespace FMS.DataLayer
 {
-    class FlightDAO
+    public class FlightDAO
     {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=HYD-1FVP2N3\SQLEXPRESS;Initial Catalog=AirplaneManagement;Integrated Security=True");
+        SqlCommand command = null;
+
+        public void AddFlight(Flight flight)
+        {
+            try
+            {
+                String query = "INSERT INTO FLIGHT (flight_id, flight_cmp, source, destination, departure_time, landing_time) VALUES" +
+                    " (@id, @cmp, @src, @dest, @dpt_time, @lndg_time)";
+                command = new SqlCommand(query, sqlConnection);
+                command.Parameters.AddWithValue("@id", flight.flight_id);
+                command.Parameters.AddWithValue("@cmp", flight.flight_cmp);
+                command.Parameters.AddWithValue("@src", flight.source);
+                command.Parameters.AddWithValue("@dest", flight.destination);
+                command.Parameters.AddWithValue("@dpt_time", flight.departure_time);
+                command.Parameters.AddWithValue("@lndg_time", flight.landing_time);
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        public List<Flight> GetAllFlights()
+        {
+            try
+            {
+                String query = "SELECT * FROM FLIGHT";
+                command = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader datareader = command.ExecuteReader();
+                List<Flight> flights = new List<Flight>();
+                if (datareader.HasRows)
+                {
+                    while (datareader.Read())
+                    {
+                        flights.Add(new Flight()
+                        {
+                            flight_id = datareader[0].ToString(),
+                            flight_cmp = datareader[1].ToString(),
+                            source = datareader[2].ToString(),
+                            destination = datareader[3].ToString(),
+                            departure_time = (DateTime)datareader[4],
+                            landing_time = (DateTime)datareader[5]
+                        });
+                    }
+                }
+                return flights;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { sqlConnection.Close(); }
+        }
+        public List<AvailableFlightWithSeat> GetFlightsBySandD(string source, string destination)
+        {
+            try
+            {
+                String query = "SELECT * FROM FLIGHT f JOIN SEAT s ON f.flight_id = seat.flight_id WHERE" +
+                    " (f.source = @src) and (f.destination = @dest) and (s.status = 0)";
+                command = new SqlCommand(query, sqlConnection);
+                command.Parameters.AddWithValue("@src", source);
+                command.Parameters.AddWithValue("@dest", destination);
+                sqlConnection.Open();
+                SqlDataReader datareader = command.ExecuteReader();
+                List<AvailableFlightWithSeat> flights = new List<AvailableFlightWithSeat>();
+                if (datareader.HasRows)
+                {
+                    while (datareader.Read())
+                    {
+
+                        flights.Add(new AvailableFlightWithSeat()
+                        {
+                            flight_id = datareader[0].ToString(),
+                            flight_cmp = datareader[1].ToString(),
+                            source = datareader[2].ToString(),
+                            destination = datareader[3].ToString(),
+                            departure_time = (DateTime)datareader[4],
+                            landing_time = (DateTime)datareader[5],
+                            seat_no = (int)datareader[7],
+                            class_type = datareader[8].ToString(),
+                            price = (float)datareader[9],
+                            status = (int)datareader[10] == 0 ? false : true
+
+                        });
+                    }
+                }
+                return flights;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { sqlConnection.Close(); }
+        }
     }
+
 }
--- backend/FMS/FMS.DataLayer/UserDAO.cs
+++ backend/FMS/FMS.DataLayer/UserDAO.cs
@@ -1,8 +1,65 @@
 ﻿using System;
+using System.Data.SqlClient;
+using FMS.Entities;
+using System.Collections.Generic;
 
 namespace FMS.DataLayer
 {
-    public class Class1
+    public class UserDAO
     {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=HYD-1FVP2N3\SQLEXPRESS;Initial Catalog=AirplaneManagement;Integrated Security=True");
+        SqlCommand command = null;
+
+        public void AddUser(User user)
+        {
+            try
+            {
+                string query = "INSERT INTO USER (username, password, seat_preference, role) VALUES (@un, @pwd, @seat_pref, @role)";
+                command = new SqlCommand(query, sqlConnection);
+                command.Parameters.AddWithValue("@un", user.username);
+                command.Parameters.AddWithValue("@pwd", user.seat_preference);
+                command.Parameters.AddWithValue("@seat_pref", user.seat_preference);
+                command.Parameters.AddWithValue("@role", user.role);
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex; 
+            }
+            finally
+            {
+                sqlConnection.Close();
+            } 
+        }
+        public bool CheckAdmin(User user)
+        {
+            try
+            {
+                string query = "SELECT * FROM User WHERE USER.USERNAME = @un";
+                command = new SqlCommand();
+                command.Parameters.AddWithValue("@un", user.username);
+                sqlConnection.Open();
+                SqlDataReader datareader = command.ExecuteReader();
+                if (datareader.HasRows)
+                {
+                    datareader.Read();
+                    if (datareader["role"].ToString() == "Admin")
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex; 
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }  
+        }
+
     }
 }
