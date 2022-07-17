19984793f20ec010a9243a06e67ae2adf5404be1
Laharish01 <laharish2k17@gmail.com>
2022-07-17 14:22:34 +0530

    added flight controller functions

--- backend/FMS/FlightService/Controllers/FlightController.cs
+++ backend/FMS/FlightService/Controllers/FlightController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FMS.Entities;
+using FMS.DataLayer;
 
 namespace FlightService.Controllers
 {
@@ -11,5 +13,56 @@ namespace FlightService.Controllers
     [ApiController]
     public class FlightController : ControllerBase
     {
+        //adding data layer object
+        public static FlightDAO flightDao;
+
+        public FlightController()
+        {
+            flightDao = new FlightDAO();
+        }
+        //get all flights
+        [HttpGet]
+        public IActionResult GetAllFlights()
+        {
+            try
+            {
+                List<Flight> allFlights = flightDao.GetAllFlights();
+                return StatusCode(200, allFlights);
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, e.Message);
+            }
+        }
+        //get flights by source and destination
+        [HttpGet]
+        public IActionResult GetFlightBySandD(string source, string destination)
+        {
+            try
+            {
+                List<AvailableFlightWithSeat> availableFlights = flightDao.GetFlightsBySandD(source, destination);
+                return StatusCode(200, availableFlights);
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+        //add flight
+        [HttpPost]
+        public IActionResult AddFlight(Flight flight)
+        {
+            try
+            {
+                flightDao.AddFlight(flight);
+                return StatusCode(200);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
     }
 }
