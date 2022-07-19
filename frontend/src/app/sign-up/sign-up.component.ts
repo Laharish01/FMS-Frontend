8b3c34a1b4d8dc2cc83e89002d256516eb32eea7
Sejal Singh <Sejal.Singh@gep.com>
2022-07-19 05:32:28 +0530

    SignUpPage

--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -14,7 +14,7 @@ export class SignUpComponent implements OnInit {
   constructor() { }
   form = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9]*$")]),
-    password: new FormControl('',[Validators.required, Validators.minLength(8),Validators.pattern("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,12}$")]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8),Validators.pattern("/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$")]),
     SeatType: new FormControl('',[Validators.required])
   })
   ngOnInit(): void {
@@ -26,5 +26,7 @@ export class SignUpComponent implements OnInit {
   submit(){
     this.submitted = true;
     console.log(this.form.value);
+    this.form.reset();
+
   }
 }
