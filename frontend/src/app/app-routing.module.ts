12fddfc126e11433480b297c1cbff63cf8142b81
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2022-07-20 11:03:47 +0530

    routing added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './Components/admin/admin.component';
+import { PaymentsComponent } from './Components/payments/payments.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', pathMatch:'full', redirectTo: 'login'},
   {path: 'signUp',component: SignUpComponent},
-  {path: 'admin', component: AdminComponent}
+  {path: 'admin', component: AdminComponent}, 
+  {path: 'payment', component:PaymentsComponent}
 ];
 
 @NgModule({
