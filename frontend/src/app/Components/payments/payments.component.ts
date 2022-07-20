56dcc599c96c4ff6fa3d2021ca3de08269a31e43
Shubham <shubham18366@iiitd.ac.in>
2022-07-20 11:00:43 +0530

    public key added for stripe

--- frontend/src/app/Components/payments/payments.component.ts
+++ frontend/src/app/Components/payments/payments.component.ts
@@ -3,51 +3,50 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
-  styleUrls: ['./payments.component.css']
+  styleUrls: ['./payments.component.css'],
 })
 export class PaymentsComponent implements OnInit {
+  paymentHandler: any = null;
 
-  paymentHandler:any = null;
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.invokeStripe();
   }
-  
+
   initializePayment(amount: number) {
     const paymentHandler = (<any>window).StripeCheckout.configure({
-      key: 'pk_test_sLUqHXtqXOkwSdPosC8ZikQ800snMatYMb',
+      key: 'pk_test_51KFG5eSBjSw1UXqU0bmIQ2IjKwPEAAtoNZDZBY8G2kbcmFRXZ6fMapBAM66AMVGQeRvhD3QarzzLEDjJejJqmQXt00QLEzsHLi',
       locale: 'auto',
       token: function (stripeToken: any) {
-        console.log({stripeToken})
+        console.log({ stripeToken });
         alert('Stripe token generated!');
-      }
+      },
     });
-  
+
     paymentHandler.open({
       name: 'FreakyJolly',
       description: 'Buying a Hot Coffee',
-      amount: amount * 100
+      amount: amount * 100,
     });
   }
-  
+
   invokeStripe() {
-    if(!window.document.getElementById('stripe-script')) {
-      const script = window.document.createElement("script");
-      script.id = "stripe-script";
-      script.type = "text/javascript";
-      script.src = "https://checkout.stripe.com/checkout.js";
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = 'https://checkout.stripe.com/checkout.js';
       script.onload = () => {
         this.paymentHandler = (<any>window).StripeCheckout.configure({
           key: 'pk_test_sLUqHXtqXOkwSdPosC8ZikQ800snMatYMb',
           locale: 'auto',
           token: function (stripeToken: any) {
-            console.log(stripeToken)
+            console.log(stripeToken);
             alert('Payment has been successfull!');
-          }
+          },
         });
-      }
+      };
       window.document.body.appendChild(script);
     }
   }
