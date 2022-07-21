8417115fe68e84bbfeda3ed9e85f3af60f557117
Laharish01 <laharish2k17@gmail.com>
2022-07-21 16:43:30 +0530

    Update admin.component.ts

--- frontend/src/app/Components/admin/admin.component.ts
+++ frontend/src/app/Components/admin/admin.component.ts
@@ -10,7 +10,7 @@ import { Availableflightwithseat } from 'src/app/Model/AvailableFlightWithSeat/a
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
   submitted = false;
@@ -21,16 +21,18 @@ export class AdminComponent implements OnInit {
   source: string;
   destination: string;
   departure_time: Date;
-  available_seats:number;
-  constructor(private flightService : FlightService, private bookingService: BookingService) {
+  available_seats: number;
+  constructor(
+    private flightService: FlightService,
+    private bookingService: BookingService
+  ) {
     this.flight = new Flight();
-    this.flightService.GetAllFlights().subscribe(response => {
+    this.flightService.GetAllFlights().subscribe((response) => {
       this.all_flights = response;
-    })
-   }
-
-  ngOnInit(): void {
+    });
   }
+
+  ngOnInit(): void {}
 
   form = new FormGroup({
     flight_id: new FormControl('', Validators.required),
@@ -38,18 +40,18 @@ export class AdminComponent implements OnInit {
     source: new FormControl('', Validators.required),
     destination: new FormControl('', Validators.required),
     departure_time: new FormControl('', Validators.required),
-    landing_time: new FormControl('', Validators.required)
+    landing_time: new FormControl('', Validators.required),
   });
 
   addseat = new FormGroup({
-    seats: new FormControl('', Validators.pattern("[E,B]\d+"))
+    seats: new FormControl('', Validators.pattern('[E,B]d+')),
   });
 
   get f() {
     return this.form.controls;
   }
 
-  get s(){
+  get s() {
     return this.addseat.controls;
   }
 
@@ -57,46 +59,47 @@ export class AdminComponent implements OnInit {
     this.submitted = true;
     console.log(this.form.value);
 
-    this.flight.flight_id =  this.form.value.flight_id;
-    this.flight.flight_cmp =  this.form.value.flight_cmp;
-    this.flight.source =  this.form.value.source;
-    this.flight.destination =  this.form.value.destination;
-    this.flight.departure_time =  new Date(this.form.value.departure_time);
+    this.flight.flight_id = this.form.value.flight_id;
+    this.flight.flight_cmp = this.form.value.flight_cmp;
+    this.flight.source = this.form.value.source;
+    this.flight.destination = this.form.value.destination;
+    this.flight.departure_time = new Date(this.form.value.departure_time);
     this.flight.landing_time = new Date(this.form.value.landing_time);
-    
-    this.flightService.AddFlight(this.flight).subscribe(response =>{
+
+    this.flightService.AddFlight(this.flight).subscribe((response) => {
       console.log(response);
     });
     this.form.reset();
   }
 
-  async addSeats(){
+  async addSeats() {
     this.rawseats = this.addseat.value.seats.split(',');
-    this.rawseats.forEach(element => {      
-      var total_no = element.substring(1, );      
-      for(var i=0;i<Number(total_no);i++){
+    this.rawseats.forEach((element) => {
+      var total_no = element.substring(1);
+      for (var i = 0; i < Number(total_no); i++) {
         var seat = new Seat();
 
         // seat.flight_id = this.flight.flight_id;
-        seat.flight_id = "6E4569"
-        seat.price = element[0] == 'E'? 3000 : 6000;
-        seat.seat_class = element[0] == 'E'? "Economy" : "Business";
-        seat.seat_no = i+1;
+        seat.flight_id = '6E4569';
+        seat.price = element[0] == 'E' ? 3000 : 6000;
+        seat.seat_class = element[0] == 'E' ? 'Economy' : 'Business';
+        seat.seat_no = i + 1;
         seat.status = false;
 
-         this.bookingService.AddSeat(seat).subscribe((response) => {
+        this.bookingService.AddSeat(seat).subscribe((response) => {
           console.log(response);
         });
       }
     });
   }
-  getFlightList(){    
-    this.flightService.GetFilteredFlights(this.source, this.destination, this.departure_time).subscribe(response => {
-      this.available_flights = response;
-    });
-    this.flightService.GetCount("6E4567").subscribe(response => {
+  getFlightList() {
+    this.flightService
+      .GetFilteredFlights(this.source, this.destination, this.departure_time)
+      .subscribe((response) => {
+        this.available_flights = response;
+      });
+    this.flightService.GetCount('6E4567').subscribe((response) => {
       this.available_seats = response;
-    })
-    
+    });
   }
 }
